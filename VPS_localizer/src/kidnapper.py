02f6b1c8882c21e5fada057a30666c119b60b960
Navifra-Rich <richard@navifra.com>
2024-03-28 16:24:19 +0900

    [style] 주석조금

--- VPS_localizer/src/kidnapper.py
+++ VPS_localizer/src/kidnapper.py
@@ -27,6 +27,10 @@ class Kidnapper:
         self.dataset_json = json.loads(open(self.dataset_path).read())
         self.testset_path = os.path.join(DATASET_TEST_PATH, "meta.json")
         self.testset_json = json.loads(open(self.testset_path).read())
+
+
+        self.originMap_image_path = os.path.join(PACKAGE_PATH, "data", "magok_superstart.png") # 결과물 출력용 지도 원본 이미지
+
         self.setKAZE()
 
     def setPubSub(self):
@@ -39,7 +43,6 @@ class Kidnapper:
         self.kaze.getImgList()
 
     def getNearestByKAZE(self, image):
-        
         places = kidnapper.kaze.save_nearest_img(image)
         return places
     
@@ -50,6 +53,7 @@ class Kidnapper:
     
     def img_callback(self, msg):
 
+        # ------------------ Image Preprocess -------------------------------------
         # image Msg에서 OpenCVImage로 변경
         img = bridge.compressed_imgmsg_to_cv2(msg)
         img = cv2.flip(img, 0)          # 원본 이미지 뒤집어져있는 이슈
@@ -59,6 +63,7 @@ class Kidnapper:
         img = cv2.resize(img, (target_width, target_height))
         # print(f"Image path {DATASET_TEST_PATH} ")
 
+        # ------------------ Detect Feature & Estimate Pose -------------------------------------
         kaze_result = kidnapper.getNearestByKAZE(img)
         print(f"KAZE RESULT {kaze_result}")
 
@@ -66,7 +71,6 @@ class Kidnapper:
         output_save_path = os.path.join(PACKAGE_PATH,"result")
         os.makedirs(output_save_path, exist_ok=True)
 
-        originMap_image_path = os.path.join(PACKAGE_PATH, "data", "magok_superstart.png")
         saveMap_image_path = os.path.join(PACKAGE_PATH, "result", "resultMap.png")
 
 
@@ -74,54 +78,53 @@ class Kidnapper:
         estimated_pose = self.dataset_json[kaze_result[0]]["odom"]
 
 
-        img_map = cv2.imread(originMap_image_path)
+        # ------------------ Print Output -------------------------------------
+        img_map = cv2.imread(self.originMap_image_path)
         # cv2.circle(img_map, (int(groundtruth_pose[0]*100 + img_map.shape[1]/2), int( -groundtruth_pose[1]*100 + img_map.shape[0]/2)) , 10, (0, 0, 255) , -1)    
         cv2.circle(img_map, (int(estimated_pose[0]*100 + img_map.shape[1]/2), int( -estimated_pose[1]*100 + img_map.shape[0]/2)) , 10, (255, 0, 0) , -1)    
-
         cv2.imwrite(saveMap_image_path, img_map)
 
         # print(f"Input  Pose  {groundtruth_pose}")
         print(f"Estimated Pose {estimated_pose}")
         print()
 
-def EstimateDatasets(msg):
-    print(f"Image path {DATASET_TEST_PATH} ")
+    def EstimateDatasets(self, msg):
+        print(f"Image path {DATASET_TEST_PATH} ")
 
-    print(f"Test Index {msg.data}")
-    kaze_result = kidnapper.getNearestByKAZE(msg.data)
-    print(f"KAZE RESULT {kaze_result}")
+        print(f"Test Index {msg.data}")
+        kaze_result = kidnapper.getNearestByKAZE(msg.data)
+        print(f"KAZE RESULT {kaze_result}")
 
-    img_input = cv2.imread(os.path.join(DATASET_TEST_PATH, str(msg.data)+".jpg"))
-    img_kaze1 = cv2.imread(os.path.join(DATASET_PATH, str(kaze_result[0])+".jpg"))
+        img_input = cv2.imread(os.path.join(DATASET_TEST_PATH, str(msg.data)+".jpg"))
+        img_kaze1 = cv2.imread(os.path.join(DATASET_PATH, str(kaze_result[0])+".jpg"))
 
-    ## PARAMETER
-    output_save_path = os.path.join(PACKAGE_PATH,"result")
-    os.makedirs(output_save_path, exist_ok=True)
-
-
-    # Save Test Image
-    cv2.imwrite(os.path.join(output_save_path,f"origin.jpg"), img_input)
-
-    # Save Result Images
-    cv2.imwrite(os.path.join(output_save_path,f"kaze1.jpg"), img_kaze1)
-
-    originMap_image_path = os.path.join(PACKAGE_PATH, "data", "magok_superstart.png")
-    saveMap_image_path = os.path.join(PACKAGE_PATH, "result", "resultMap.png")
+        ## PARAMETER
+        output_save_path = os.path.join(PACKAGE_PATH,"result")
+        os.makedirs(output_save_path, exist_ok=True)
 
 
-    groundtruth_pose = kidnapper.testset_json[msg.data]["odom"]
-    estimated_pose = kidnapper.dataset_json[kaze_result[0]]["odom"]
+        # Save Test Image
+        cv2.imwrite(os.path.join(output_save_path,f"origin.jpg"), img_input)
+
+        # Save Result Images
+        cv2.imwrite(os.path.join(output_save_path,f"kaze1.jpg"), img_kaze1)
+
+        saveMap_image_path = os.path.join(PACKAGE_PATH, "result", "resultMap.png")
 
 
-    img_map = cv2.imread(originMap_image_path)
-    cv2.circle(img_map, (int(groundtruth_pose[0]*100 + img_map.shape[1]/2), int( -groundtruth_pose[1]*100 + img_map.shape[0]/2)) , 10, (0, 0, 255) , -1)    
-    cv2.circle(img_map, (int(estimated_pose[0]*100 + img_map.shape[1]/2), int( -estimated_pose[1]*100 + img_map.shape[0]/2)) , 10, (255, 0, 0) , -1)    
+        groundtruth_pose = kidnapper.testset_json[msg.data]["odom"]
+        estimated_pose = kidnapper.dataset_json[kaze_result[0]]["odom"]
 
-    cv2.imwrite(saveMap_image_path, img_map)
 
-    print(f"Input  Pose  {groundtruth_pose}")
-    print(f"Output Pose1 {estimated_pose}")
-    print()
+        img_map = cv2.imread(self.originMap_image_path)
+        cv2.circle(img_map, (int(groundtruth_pose[0]*100 + img_map.shape[1]/2), int( -groundtruth_pose[1]*100 + img_map.shape[0]/2)) , 10, (0, 0, 255) , -1)    
+        cv2.circle(img_map, (int(estimated_pose[0]*100 + img_map.shape[1]/2), int( -estimated_pose[1]*100 + img_map.shape[0]/2)) , 10, (255, 0, 0) , -1)    
+
+        cv2.imwrite(saveMap_image_path, img_map)
+
+        print(f"Input  Pose  {groundtruth_pose}")
+        print(f"Output Pose1 {estimated_pose}")
+        print()
 
 
 kidnapper = Kidnapper()
